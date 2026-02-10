33843b1d52b4e965ce572e1a1de014edb7bca187
souvikjs01 <souvik741156@gmail.com>
2026-02-10 19:44:26 +0530

    user model & methods

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,46 @@
 package models
 
+import (
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	UserID    string `json:"user_id"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Role      string `json:"role"`
-	Avatar    string `json:"avatar"`
-	Password  string `json:"password"`
+	UserID    string    `json:"user_id" db:"user_id" validate:"omitempty"`
+	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
+	FirstName string    `json:"first_name" db:"first_name" validate:"required,lte=30"`
+	LastName  string    `json:"last_name" db:"last_name" validate:"required,lte=30"`
+	Role      string    `json:"role" db:"role"`
+	Avatar    string    `json:"avatar"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
+}
+
+func (u *User) HashPassword() error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashed)
+	return nil
+}
+
+func (u *User) ComparePasswords(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+}
+
+func (u *User) PrepareCreate() error {
+	u.Email = strings.ToLower(strings.TrimSpace(u.Email))
+	u.Password = strings.TrimSpace(u.Password)
+
+	if err := u.HashPassword(); err != nil {
+		return err
+	}
+	if u.Role != "" {
+		u.Role = strings.TrimSpace(u.Role)
+	}
+	return nil
 }
