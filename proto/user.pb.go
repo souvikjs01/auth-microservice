c051b3916aaaa6a0e69873243cc33147715e5e70
souvikjs01 <souvik741156@gmail.com>
2026-02-11 10:34:33 +0530

    update proto

--- proto/user.pb.go
+++ proto/user.pb.go
@@ -302,6 +302,182 @@ func (x *RegisterResponse) GetUser() *User {
 	return nil
 }
 
+type FindByEmailRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Email         string                 `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *FindByEmailRequest) Reset() {
+	*x = FindByEmailRequest{}
+	mi := &file_user_proto_msgTypes[4]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *FindByEmailRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FindByEmailRequest) ProtoMessage() {}
+
+func (x *FindByEmailRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[4]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FindByEmailRequest.ProtoReflect.Descriptor instead.
+func (*FindByEmailRequest) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *FindByEmailRequest) GetEmail() string {
+	if x != nil {
+		return x.Email
+	}
+	return ""
+}
+
+type FindByEmailResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	User          *User                  `protobuf:"bytes,1,opt,name=user,proto3" json:"user,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *FindByEmailResponse) Reset() {
+	*x = FindByEmailResponse{}
+	mi := &file_user_proto_msgTypes[5]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *FindByEmailResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FindByEmailResponse) ProtoMessage() {}
+
+func (x *FindByEmailResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[5]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FindByEmailResponse.ProtoReflect.Descriptor instead.
+func (*FindByEmailResponse) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *FindByEmailResponse) GetUser() *User {
+	if x != nil {
+		return x.User
+	}
+	return nil
+}
+
+type FindByIDRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Uid           string                 `protobuf:"bytes,1,opt,name=uid,proto3" json:"uid,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *FindByIDRequest) Reset() {
+	*x = FindByIDRequest{}
+	mi := &file_user_proto_msgTypes[6]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *FindByIDRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FindByIDRequest) ProtoMessage() {}
+
+func (x *FindByIDRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[6]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FindByIDRequest.ProtoReflect.Descriptor instead.
+func (*FindByIDRequest) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *FindByIDRequest) GetUid() string {
+	if x != nil {
+		return x.Uid
+	}
+	return ""
+}
+
+type FindByIDResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	User          *User                  `protobuf:"bytes,1,opt,name=user,proto3" json:"user,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *FindByIDResponse) Reset() {
+	*x = FindByIDResponse{}
+	mi := &file_user_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *FindByIDResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FindByIDResponse) ProtoMessage() {}
+
+func (x *FindByIDResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FindByIDResponse.ProtoReflect.Descriptor instead.
+func (*FindByIDResponse) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *FindByIDResponse) GetUser() *User {
+	if x != nil {
+		return x.User
+	}
+	return nil
+}
+
 var File_user_proto protoreflect.FileDescriptor
 
 const file_user_proto_rawDesc = "" +
@@ -330,9 +506,19 @@ const file_user_proto_rawDesc = "" +
 	"\x04role\x18\x05 \x01(\tR\x04role\x12\x16\n" +
 	"\x06avatar\x18\x06 \x01(\tR\x06avatar\"9\n" +
 	"\x10RegisterResponse\x12%\n" +
-	"\x04user\x18\x01 \x01(\v2\x11.userService.UserR\x04user2V\n" +
+	"\x04user\x18\x01 \x01(\v2\x11.userService.UserR\x04user\"*\n" +
+	"\x12FindByEmailRequest\x12\x14\n" +
+	"\x05email\x18\x01 \x01(\tR\x05email\"<\n" +
+	"\x13FindByEmailResponse\x12%\n" +
+	"\x04user\x18\x01 \x01(\v2\x11.userService.UserR\x04user\"#\n" +
+	"\x0fFindByIDRequest\x12\x10\n" +
+	"\x03uid\x18\x01 \x01(\tR\x03uid\"9\n" +
+	"\x10FindByIDResponse\x12%\n" +
+	"\x04user\x18\x01 \x01(\v2\x11.userService.UserR\x04user2\xf1\x01\n" +
 	"\vUserService\x12G\n" +
-	"\bRegister\x12\x1c.userService.RegisterRequest\x1a\x1d.userService.RegisterResponseB\x0fZ\r.;userServiceb\x06proto3"
+	"\bRegister\x12\x1c.userService.RegisterRequest\x1a\x1d.userService.RegisterResponse\x12P\n" +
+	"\vFindByEmail\x12\x1f.userService.FindByEmailRequest\x1a .userService.FindByEmailResponse\x12G\n" +
+	"\bFindByID\x12\x1c.userService.FindByIDRequest\x1a\x1d.userService.FindByIDResponseB\x0fZ\r.;userServiceb\x06proto3"
 
 var (
 	file_user_proto_rawDescOnce sync.Once
@@ -346,25 +532,35 @@ func file_user_proto_rawDescGZIP() []byte {
 	return file_user_proto_rawDescData
 }
 
-var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_user_proto_goTypes = []any{
 	(*Session)(nil),               // 0: userService.Session
 	(*User)(nil),                  // 1: userService.User
 	(*RegisterRequest)(nil),       // 2: userService.RegisterRequest
 	(*RegisterResponse)(nil),      // 3: userService.RegisterResponse
-	(*timestamppb.Timestamp)(nil), // 4: google.protobuf.Timestamp
+	(*FindByEmailRequest)(nil),    // 4: userService.FindByEmailRequest
+	(*FindByEmailResponse)(nil),   // 5: userService.FindByEmailResponse
+	(*FindByIDRequest)(nil),       // 6: userService.FindByIDRequest
+	(*FindByIDResponse)(nil),      // 7: userService.FindByIDResponse
+	(*timestamppb.Timestamp)(nil), // 8: google.protobuf.Timestamp
 }
 var file_user_proto_depIdxs = []int32{
-	4, // 0: userService.User.createdAt:type_name -> google.protobuf.Timestamp
-	4, // 1: userService.User.updatedAt:type_name -> google.protobuf.Timestamp
+	8, // 0: userService.User.createdAt:type_name -> google.protobuf.Timestamp
+	8, // 1: userService.User.updatedAt:type_name -> google.protobuf.Timestamp
 	1, // 2: userService.RegisterResponse.user:type_name -> userService.User
-	2, // 3: userService.UserService.Register:input_type -> userService.RegisterRequest
-	3, // 4: userService.UserService.Register:output_type -> userService.RegisterResponse
-	4, // [4:5] is the sub-list for method output_type
-	3, // [3:4] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	1, // 3: userService.FindByEmailResponse.user:type_name -> userService.User
+	1, // 4: userService.FindByIDResponse.user:type_name -> userService.User
+	2, // 5: userService.UserService.Register:input_type -> userService.RegisterRequest
+	4, // 6: userService.UserService.FindByEmail:input_type -> userService.FindByEmailRequest
+	6, // 7: userService.UserService.FindByID:input_type -> userService.FindByIDRequest
+	3, // 8: userService.UserService.Register:output_type -> userService.RegisterResponse
+	5, // 9: userService.UserService.FindByEmail:output_type -> userService.FindByEmailResponse
+	7, // 10: userService.UserService.FindByID:output_type -> userService.FindByIDResponse
+	8, // [8:11] is the sub-list for method output_type
+	5, // [5:8] is the sub-list for method input_type
+	5, // [5:5] is the sub-list for extension type_name
+	5, // [5:5] is the sub-list for extension extendee
+	0, // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_user_proto_init() }
@@ -378,7 +574,7 @@ func file_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_user_proto_rawDesc), len(file_user_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- proto/user_grpc.pb.go
+++ proto/user_grpc.pb.go
@@ -19,7 +19,9 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	UserService_Register_FullMethodName = "/userService.UserService/Register"
+	UserService_Register_FullMethodName    = "/userService.UserService/Register"
+	UserService_FindByEmail_FullMethodName = "/userService.UserService/FindByEmail"
+	UserService_FindByID_FullMethodName    = "/userService.UserService/FindByID"
 )
 
 // UserServiceClient is the client API for UserService service.
@@ -27,6 +29,8 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type UserServiceClient interface {
 	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
+	FindByEmail(ctx context.Context, in *FindByEmailRequest, opts ...grpc.CallOption) (*FindByEmailResponse, error)
+	FindByID(ctx context.Context, in *FindByIDRequest, opts ...grpc.CallOption) (*FindByIDResponse, error)
 }
 
 type userServiceClient struct {
@@ -47,11 +51,33 @@ func (c *userServiceClient) Register(ctx context.Context, in *RegisterRequest, o
 	return out, nil
 }
 
+func (c *userServiceClient) FindByEmail(ctx context.Context, in *FindByEmailRequest, opts ...grpc.CallOption) (*FindByEmailResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(FindByEmailResponse)
+	err := c.cc.Invoke(ctx, UserService_FindByEmail_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *userServiceClient) FindByID(ctx context.Context, in *FindByIDRequest, opts ...grpc.CallOption) (*FindByIDResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(FindByIDResponse)
+	err := c.cc.Invoke(ctx, UserService_FindByID_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility.
 type UserServiceServer interface {
 	Register(context.Context, *RegisterRequest) (*RegisterResponse, error)
+	FindByEmail(context.Context, *FindByEmailRequest) (*FindByEmailResponse, error)
+	FindByID(context.Context, *FindByIDRequest) (*FindByIDResponse, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -64,6 +90,12 @@ type UnimplementedUserServiceServer struct{}
 
 func (UnimplementedUserServiceServer) Register(context.Context, *RegisterRequest) (*RegisterResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
+}
+func (UnimplementedUserServiceServer) FindByEmail(context.Context, *FindByEmailRequest) (*FindByEmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindByEmail not implemented")
+}
+func (UnimplementedUserServiceServer) FindByID(context.Context, *FindByIDRequest) (*FindByIDResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindByID not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 func (UnimplementedUserServiceServer) testEmbeddedByValue()                     {}
@@ -104,6 +136,42 @@ func _UserService_Register_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_FindByEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindByEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).FindByEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: UserService_FindByEmail_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).FindByEmail(ctx, req.(*FindByEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _UserService_FindByID_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindByIDRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).FindByID(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: UserService_FindByID_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).FindByID(ctx, req.(*FindByIDRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +182,14 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Register",
 			Handler:    _UserService_Register_Handler,
+		},
+		{
+			MethodName: "FindByEmail",
+			Handler:    _UserService_FindByEmail_Handler,
+		},
+		{
+			MethodName: "FindByID",
+			Handler:    _UserService_FindByID_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
